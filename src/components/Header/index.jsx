d55819f404620396d1b9e7b0b96ea73eaa88b8e4
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-11-24 14:27:51 -0300

    header e profiles update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,15 @@
-import { Container } from "./style";
+import { Container, Profile } from "./style";
 
 export function Header() {
-  return <Container>teste</Container>;
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/eduardoB7.png" alt="foto do usuario" />
+        <div>
+          <span>Bem vindo</span>
+          <strong>Eduardo Barroso</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }
