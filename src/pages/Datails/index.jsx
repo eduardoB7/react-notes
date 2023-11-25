8377a8cb04e1e2febf62d4a07ed84dc3fe58fc51
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-11-24 21:01:28 -0300

    update no header e componete section

--- src/pages/Datails/index.jsx
+++ src/pages/Datails/index.jsx
@@ -1,11 +1,27 @@
+import { Container, Links } from "./styles";
+
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import { Section } from "../../components/Section";
 
 export function Datails() {
   return (
     <Container>
       <Header />
+      <Section title={"Links úteis"}>
+        <Links>
+          <li>
+            <a target="_blank" href="https://app.rocketseat.com.br/">
+              https//:www.rocketseat.com.br
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://app.rocketseat.com.br/">
+              https//:www.rocketseat.com.br
+            </a>
+          </li>
+        </Links>
+      </Section>
       <Button textContent={"Voltar"} />
     </Container>
   );
