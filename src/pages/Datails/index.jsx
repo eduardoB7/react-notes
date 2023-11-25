d65952292867bfce050f36fbc4511d3334e8764a
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-11-24 22:39:12 -0300

    ajutes no details

--- src/pages/Datails/index.jsx
+++ src/pages/Datails/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
@@ -10,26 +10,42 @@ export function Datails() {
   return (
     <Container>
       <Header />
-      <ButtonText texContext={"Excluir a nota"} />
-      <Section title={"Links úteis"}>
-        <Links>
-          <li>
-            <a target="_blank" href="https://app.rocketseat.com.br/">
-              https//:www.rocketseat.com.br
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://app.rocketseat.com.br/">
-              https//:www.rocketseat.com.br
-            </a>
-          </li>
-        </Links>
-      </Section>
-      <Section title={"Marcadores"}>
-        <Tag texContent={"Express"} />
-        <Tag texContent={"Nodejs"} />
-      </Section>
-      <Button textContent={"Voltar"} />
+      <main>
+        <Content>
+          <ButtonText texContext={"Excluir a nota"} />
+
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+            illum et fugiat? Maxime doloremque unde illum sunt doloribus
+            molestias, neque tenetur deleniti commodi nulla dolore earum quae
+            debitis nihil quaerat! Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Dolorum illum et fugiat? Maxime doloremque unde
+            illum sunt doloribus molestias, neque tenetur deleniti commodi nulla
+            dolore earum quae debitis nihil quaerat!
+          </p>
+          <Section title={"Links úteis"}>
+            <Links>
+              <li>
+                <a target="_blank" href="https://app.rocketseat.com.br/">
+                  https//:www.rocketseat.com.br
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://app.rocketseat.com.br/">
+                  https//:www.rocketseat.com.br
+                </a>
+              </li>
+            </Links>
+          </Section>
+          <Section title={"Marcadores"}>
+            <Tag texContent={"Express"} />
+            <Tag texContent={"Nodejs"} />
+          </Section>
+
+          <Button textContent={"Voltar"} />
+        </Content>
+      </main>
     </Container>
   );
 }
