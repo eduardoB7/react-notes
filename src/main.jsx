579f65fba48ff364e77b89b61085eb6895ae9e05
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-11-29 16:03:36 -0300

    SingIn page

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
-import { Home } from "./pages/Home";
+import { SignIn } from "./pages/Signin";
 import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,24 @@
+import { Container, Form } from "./style";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="Email" type="text" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button textContent={"Entrar"} />
+
+        <a href="#">Criar conta</a>
+      </Form>
+    </Container>
+  );
+}
