042af0a97893157886b3b44bd18e82aa470b13f0
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-11-29 16:41:44 -0300

    SignUp page

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
-import { SignIn } from "./pages/Signin";
+import { SignUp } from "./pages/SignUp";
 import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
