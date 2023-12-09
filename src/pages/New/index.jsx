ca86c8ecd8fc22567e3ccbfe1d1f2789e7fae36c
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-12-09 15:51:31 -0300

    ajuste

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,11 @@
 import { Container, FormNew } from "./style";
 
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
-import { Textarea } from "../../components/Texarea";
 import { NoteItem } from "../../components/NoteItem";
+import { Textarea } from "../../components/Texarea";
 import { Section } from "../../components/Section";
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
 export function New() {
   return (
